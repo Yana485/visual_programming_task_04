f84e068d0fd55e147e625acaf19be2fc25b170e4
Yana485 <105270905+Yana485@users.noreply.github.com>
2024-06-08 15:11:02 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public void ToolbarSample()
+        {
+            InitializeComponent();
+        }
+
+        private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+            int col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
+            lblCursorPosition.Text = "Line " + (row + 1) + ", Char " + (col + 1);
+        }
+
+    }
+
+}
